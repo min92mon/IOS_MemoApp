f6f3b372d563683fdd8b0d5687dbd899fccca596
min92mon <min92mon@gmail.com>
2021-07-29 23:27:55 +0900

    Initial Commit

---
+++ MyMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by 정민영 on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
