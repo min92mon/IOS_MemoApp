8487cd40b30732500104affde6c2562bcbf61dab
min92mon <min92mon@gmail.com>
2021-08-02 03:01:22 +0900

    Implement MemoWriteView feature - Implement Pick Image Button - Implement Save Button - Implement TextView

--- MyMemo/MemoWriteVC.swift
+++ MyMemo/MemoWriteVC.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class MemoWriteVC: UIViewController {
+class MemoWriteVC: UIViewController, UINavigationControllerDelegate {
+    var subject: String!
+    
+    @IBOutlet weak var contents: UITextView!
+    @IBOutlet weak var preview: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.contents.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func save(_ sender: Any) {
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let data = MemoData()
+        
+        data.title = self.subject
+        data.contents = self.contents.text
+        data.image = self.preview.image
+        data.regdate = Date()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memoList.append(data)
+        
+        _ = self.navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    @IBAction func pick(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        self.present(picker, animated: false)
+    }
 }
+
+// Mark:-UIImagePickerController Delegate Implement
+extension MemoWriteVC: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        self.preview.image = info[.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false, completion: nil)
+    }
+}
+
+// Mark:-UITextView Delegate Implement
+extension MemoWriteVC: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let contents = textView.text as NSString
+        let length = ( (contents.length > 15) ? 15 : contents.length )
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        self.navigationItem.title = self.subject
+    }
+}
+
