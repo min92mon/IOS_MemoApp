a48f796b4b31841165477e4d0386c448eb17bd4b
min92mon <min92mon@gmail.com>
2021-08-02 02:16:55 +0900

    Make ViewClass and Link Class

--- MyMemo/AppDelegate.swift
+++ MyMemo/AppDelegate.swift
@@ -10,8 +10,7 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var memoList = [MemoData]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ MyMemo/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MemoData
+//
+//  Created by 정민영 on 2021/08/02.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Date?
+}
